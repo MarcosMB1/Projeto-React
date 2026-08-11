8355163f3f17c93d20d5ebad67a272db26ef3ada
MarcosMB1 <Marcoseduardomendes860@gmail.com>
2026-08-11 10:58:28 -0300

    Add package.json and package-lock.json for prettier-plugin-tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,28 @@
 import "./App.css";
+import Botao from "./components/botao";
 
 function App() {
   return (
-    <main className="flex items-center justify-center bg-
-    [#f3f3f3] min-h-screen">
-      <div className="bg-white shaddow-lg rounded-x1 p-8">
-        <h1 className="text-5xl font-bold
-        text-cyan-600">Hello, World!!!</h1>
-        <p className="mt-3 text-cyan-400">Bem vindo ao 
-          meu React com Tailwind CSS!</p>
+    <main className="flex min-h-screen items-center justify-center bg-[#252525]">
+      <div className="rounded-xl bg-white p-5 shadow-lg">
+        <h1 className="text-center text-5xl font-bold text-cyan-600">
+          Hello, World!!!
+        </h1>
+        <p className="mt-3 text-center text-cyan-400">
+          Bem vindo ao meu React com Tailwind CSS!
+        </p>
+        <div className="mt-5 flex justify-center gap-2">
+          <Botao
+            texto="Comprar"
+            cor="bg-cyan-800"
+            corHover="hover:bg-cyan-600"
+          ></Botao>
+          <Botao
+            texto="Vender"
+            cor="bg-red-800"
+            corHover="hover:bg-red-600"
+          ></Botao>
+        </div>
       </div>
     </main>
   );
